f7bf8301235cd89ec988c008ca51a429d2662bd7
carlos garcía mena <karlogm7@hotmail.com>
2022-04-20 17:17:19 -0600

    Tabla Solicitud nueva

---
+++ BaseDatos/dbo/StoreProcedures/SolicitudInsertar.sql
@@ -0,0 +1,49 @@
+﻿CREATE PROCEDURE [dbo].[SolicitudInsertar]
+	@IdCliente INT NOT NULL,
+	@IdServicio INt NOT NULL,
+	@Cantidad  INT NULL,
+	@Monto DECIMAL (18, 2),
+	@FechaEntrega datetime  NULL,
+	@UsuarioEntrega  VARCHAR (50) null,
+	@Observaciones VARCHAR (250) null
+AS
+	BEGIN
+		SET NOCOUNT ON
+
+		BEGIN TRANSACTION TRASA
+
+		BEGIN TRY
+
+		INSERT INTO dbo.Solicitud
+		(
+			
+		IdCliente
+		, IdServicio
+		, Cantidad
+		, Monto
+		, FechaEntrega
+		, UsuarioEntrega
+		, Observaciones
+		)
+		VALUES
+		(
+		@IdCliente
+		, @IdServicio
+		, @Cantidad
+		, @Monto
+		, @FechaEntrega
+		, @UsuarioEntrega
+		, @Observaciones
+		)
+		COMMIT TRANSACTION TRASA
+		SELECT 0 AS CodError, '' AS MsgError
+
+		END TRY
+
+		BEGIN CATCH
+			SELECT
+			ERROR_NUMBER() AS CodError,
+			ERROR_MESSAGE() AS MsgError
+		ROLLBACK TRANSACTION TRASA
+		END CATCH
+		END
---
+++ WBL/ISolicitudService.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public interface ISolicitudService
+    {
+        Task<DBEntity> CREATE(SolicitudEntity entity);
+        Task<DBEntity> DELETE(SolicitudEntity entity);
+        Task<IEnumerable<SolicitudEntity>> GET();
+        Task<SolicitudEntity> GETBYID(SolicitudEntity entity);
+        Task<DBEntity> UPDATE(SolicitudEntity entity);
+    }
+}
